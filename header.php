53224aef314ec7f529a7a7d20ad92814e12e8c8b
ferror <zmalcherczyk@gmail.com>
2020-02-23 22:44:42 +0100

    init grave wp theme

---
+++ header.php
@@ -0,0 +1,5 @@
+<head>
+    <title>Grave wordpress theme</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/ferror/grave@gh-pages/3.0/grave.min.css">
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+</head>
