ac124bc4847c7c147bc632a2a019ab60a1def22f
ferror <zmalcherczyk@gmail.com>
2020-02-26 19:34:50 +0100

    Add post templates

---
+++ author.php
@@ -0,0 +1,29 @@
+<html lang="<?php get_locale() ?>">
+<?php get_header(); ?>
+<body>
+    <?php get_sidebar(); ?>
+
+    <main>
+        <about>
+            <figure>
+                <img src="https://assets.malcherczyk.com/icons/512/icon_round_projects.svg" alt="Project's figure">
+                <figcaption>Zbigniew (Ferror) Malcherczyk</figcaption>
+            </figure>
+            <article>
+                <header>
+                    <h2>About header</h2>
+                    <h4>subheader about</h4>
+                </header>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus nec sollicitudin arcu. Mauris semper ut est vitae mattis. Phasellus hendrerit cursus libero, ut lobortis orci pretium sit amet. Integer finibus, justo accumsan sagittis fermentum, magna ex pretium turpis, id imperdiet orci ligula sed quam. In vulputate, libero non suscipit aliquet, purus est aliquet risus, nec scelerisque nisl
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus nec sollicitudin arcu. Mauris semper ut est vitae mattis. Phasellus hendrerit cursus libero, ut lobortis orci pretium sit amet. Integer finibus, justo accumsan sagittis fermentum, magna ex pretium turpis, id imperdiet orci ligula sed quam. In vulputate, libero non suscipit aliquet, purus est aliquet risus, nec scelerisque nisl
+                </p>
+            </article>
+        </about>
+    </main>
+
+    <?php get_footer(); ?>
+</body>
+</html>
---
+++ page.php
@@ -0,0 +1,21 @@
+<html lang="<?php get_locale() ?>">
+<?php get_header(); ?>
+<body>
+    <?php get_sidebar(); ?>
+
+    <h1 style="color: #F23456">Page template</h1>
+
+    <main>
+        <article>
+            <header>
+                <h1><?php the_title(); ?></h1>
+            </header>
+
+            <?php the_post(); ?>
+            <?php the_content() ?>
+        </article>
+    </main>
+
+    <?php get_footer(); ?>
+</body>
+</html>
