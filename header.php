bdd0c312830eb37717d5d909fd7039a7428da875
ferror <zmalcherczyk@gmail.com>
2020-03-19 21:35:07 +0100

    update grave to 3.2

--- header.php
+++ header.php
@@ -2,6 +2,6 @@
     <title>Grave wordpress theme</title>
     <meta charset="utf-8">
     <meta name="author" content="Zbigniew Malcherczyk">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/ferror/grave@gh-pages/3.0/grave.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/ferror/grave@gh-pages/3.2/grave.min.css">
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
 </head>
